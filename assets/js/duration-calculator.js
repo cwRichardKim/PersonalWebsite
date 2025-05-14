fbfd17518eccd8e8f5d44fad0320ba64c9792953
richard_kim <richard.kim@airbnb.com>
2025-05-14 18:19:55 +0200

    min count

--- assets/js/duration-calculator.js
+++ assets/js/duration-calculator.js
@@ -3,8 +3,8 @@ function calculateDuration(startDateStr) {
   const startMonth = new Date(Date.parse(startParts[0] + " 1, " + startParts[1]));
   const now = new Date();
   
-  const monthsDiff = (now.getFullYear() - startMonth.getFullYear()) * 12 + 
-                    (now.getMonth() - startMonth.getMonth());
+  const monthsDiff = Math.max((now.getFullYear() - startMonth.getFullYear()) * 12 + 
+                    (now.getMonth() - startMonth.getMonth()), 1);
   
   if (monthsDiff < 12) {
     return `(${monthsDiff} month${monthsDiff === 1 ? '' : 's'})`;
